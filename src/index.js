35c5ca54b2ecceba4b92a0a70fd2e0be039a6a12
willard-wrt <119113615+willard-wrt@users.noreply.github.com>
2023-05-01 01:18:25 +0700

    Add code that toggle 'active' class for top-nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.log('webpack is working');
+
+// const addActiveClass = (e) => {};
+
+const topNav = document.getElementById('top-nav');
+topNav.addEventListener('click', (e) => {
+  const navItem = e.target.closest('.nav.item');
+  navItem.classList.toggle('active');
+  console.log(navItem);
+});
