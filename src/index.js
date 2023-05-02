e91017c1f65c551a851338de42b8982a65e1b074
willard-wrt <119113615+willard-wrt@users.noreply.github.com>
2023-05-03 04:51:33 +0700

    Add a function to toggle an active indicator

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
 const imgController = (() => {
   const flowers = document.getElementsByClassName('flower-img');
+  const indicators = document.getElementsByClassName('position-indicator');
   const flowersLength = flowers.length;
+  let resetIndex = 0;
   let activeIndex = 0;
   function activeReset() {
     Array.from(flowers).forEach((flower) => {
       if (flower.classList.contains('active')) {
-        activeIndex = parseInt(flower.dataset.imgIndex);
+        resetIndex = parseInt(flower.dataset.imgIndex);
         flower.classList.remove('active');
       }
     });
   }
   function leftSlider() {
     activeReset();
-    if (activeIndex == 0) {
+    if (resetIndex == 0) {
       Array.from(flowers)[flowersLength - 1].classList.add('active');
+      activeIndex = flowersLength - 1;
+      activeIndicator();
     } else {
-      Array.from(flowers)[activeIndex - 1].classList.add('active');
+      Array.from(flowers)[resetIndex - 1].classList.add('active');
+      activeIndex = resetIndex - 1;
+      activeIndicator();
     }
   }
   function rightSlider() {
     activeReset();
-    Array.from(flowers)[(activeIndex + 1) % flowersLength].classList.add(
+    Array.from(flowers)[(resetIndex + 1) % flowersLength].classList.add(
       'active'
     );
+    activeIndex = (resetIndex + 1) % flowersLength;
+    activeIndicator();
+  }
+  function activeIndicator() {
+    Array.from(indicators).forEach((indicator) => {
+      if (activeIndex !== indicator.dataset.navIndex) {
+        indicator.classList.remove('active');
+      }
+      if (activeIndex == indicator.dataset.navIndex) {
+        indicator.classList.add('active');
+      }
+    });
   }
   return { leftSlider, rightSlider };
 })();
